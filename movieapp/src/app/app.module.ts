aa025151866ff1f55dcb03ba35655f97fd0e1364
Burak Özkan <burak.ozkan@path.com.tr>
2021-11-11 00:20:20 +0300

    components ready

--- movieapp/src/app/app.module.ts
+++ movieapp/src/app/app.module.ts
@@ -3,16 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CategoryComponent } from './category/category.component';
+import { MoviesComponent } from './movies/movies.component';
+import { MovieComponent } from './movies/movie/movie.component';
+import { MovieDetailsComponent } from './movies/movie-details/movie-details.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ // components
+    AppComponent,
+    NavbarComponent,
+    CategoryComponent,
+    MoviesComponent,
+    MovieComponent,
+    MovieDetailsComponent,
+    FooterComponent
   ],
-  imports: [
+  imports: [ // modules
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [], // services
+  bootstrap: [AppComponent] // starter component
 })
 export class AppModule { }
