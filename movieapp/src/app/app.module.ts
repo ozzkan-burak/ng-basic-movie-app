d4e71c10b1b0abb1b442af00a976db35d66edaa0
Burak Özkan <burak.ozkan@path.com.tr>
2022-02-16 23:59:21 +0300

    episode23

--- movieapp/src/app/app.module.ts
+++ movieapp/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MovieFilterPipe } from './pipes/movie-filter.pipe';
 import { AlertifyService } from './services/alertify.service';
 import { HttpClientModule } from '@angular/common/http';
 import { MovieCreateComponent } from './movie-create/movie-create.component';
+import { CategoryCreateComponent } from './category-create/category-create.component';
 
 @NgModule({
   declarations: [ // components
@@ -26,6 +27,7 @@ import { MovieCreateComponent } from './movie-create/movie-create.component';
     FooterComponent,
     MovieFilterPipe,
     MovieCreateComponent,
+    CategoryCreateComponent,
   ],
   imports: [ // modules
     BrowserModule,
