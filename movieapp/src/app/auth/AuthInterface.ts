edaefc188011ffdff59b57548dfc7b32de618f51
Burak ÖZKAN <burak.ozkan@flo.com.tr>
2022-04-06 23:24:52 +0300

    episode28

---
+++ movieapp/src/app/auth/AuthInterface.ts
@@ -0,0 +1,7 @@
+export interface IAuthResponse {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+}
--- movieapp/src/app/services/auth.service.ts
+++ movieapp/src/app/services/auth.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+// interface
+import { IAuthResponse } from '../auth/AuthInterface';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  
+  
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  signUp(email: string, password: string) {
+    return this.http.post<IAuthResponse>(this.url,{
+      email,
+      password,
+      returnSecureToken: true
+    })
+  }
 }
